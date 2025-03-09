32402b0d07814ad3e09311f4165bb356cb9709be
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2025-03-09 10:44:11 -0400

    working on tests

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="ctk-gui-components",  # Unique package name
-    version="0.1.5",    # Package version
+    version="0.1.7",    # Package version
     author="Amit Kshirsagar",
     author_email="devopsnextgenx@gmail.com",
     description="A collection of custom GUI components for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/devopsnextgenx/ctk-gui-components",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "customtkinter",
         "ttkbootstrap"
@@ -18,7 +19,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
         "License :: Other/Proprietary License",
- ],
+    ],
     python_requires=">=3.8",
     license="SLA",
     license_files=["LICENSE"],
